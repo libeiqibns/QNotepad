0400b25aa4c73e09fb41c4deaeb6ba2ca8391961
Beiqi Li <beiqi.li@mail.utoronto.ca>
2019-07-17 11:20:20 -0400

    add edit features

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,76 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QDir>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    open_file = "";
     ui->setupUi(this);
+    setCentralWidget(ui->textEdit);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionAbout_QNotePad_triggered()
+{
+
+}
+
+void MainWindow::on_actionselect_all_Ctrl_A_triggered()
+{
+    ui->textEdit->selectAll();
+}
+
+void MainWindow::on_actionCut_Ctrl_X_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionCopy_Ctrl_C_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_Ctrl_V_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionUndo_Ctrl_Z_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_Shift_Ctrl_Z_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void MainWindow::on_actionNew_Ctrl_N_triggered()
+{
+    open_file = "";
+    ui->textEdit->setPlainText("");
+}
+
+void MainWindow::on_actionOpen_Shift_Ctrl_N_triggered()
+{
+
+}
+
+void MainWindow::on_actionSave_Ctrl_S_triggered()
+{
+
+}
+
+void MainWindow::on_actionSave_as_Shift_Ctrl_S_triggered()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,32 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionAbout_QNotePad_triggered();
+
+    void on_actionselect_all_Ctrl_A_triggered();
+
+    void on_actionCut_Ctrl_X_triggered();
+
+    void on_actionCopy_Ctrl_C_triggered();
+
+    void on_actionPaste_Ctrl_V_triggered();
+
+    void on_actionUndo_Ctrl_Z_triggered();
+
+    void on_actionRedo_Shift_Ctrl_Z_triggered();
+
+    void on_actionNew_Ctrl_N_triggered();
+
+    void on_actionOpen_Shift_Ctrl_N_triggered();
+
+    void on_actionSave_Ctrl_S_triggered();
+
+    void on_actionSave_as_Shift_Ctrl_S_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QString open_file;
 };
 
 #endif // MAINWINDOW_H
